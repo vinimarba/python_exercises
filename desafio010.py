c16da3f32dcaa62008b85dfac891bedf4eb4caea
vinimarba <157657138+vinimarba@users.noreply.github.com>
2025-02-14 11:57:24 -0300

    desafio010.py

--- desafio010.py
+++ desafio010.py
@@ -1,6 +1,6 @@
-carteira = float(input('Digite a seguir o valor que você tem em carteira: '))
+carteira = float(input('Digite a seguir o valor que você tem em carteira: R$'))
 conversao = carteira*5.77
 
 #print('{} em carteira equivale a {} dólares'.format(carteira, conversao))
 
-print(f'{carteira} em carteira equivale a {conversao:.2f} dólares.')
+print(f'R${carteira} em carteira equivale a US${conversao:.2f}.')
